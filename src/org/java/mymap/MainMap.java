8bfd67539cb513a30458855b5cdd7ef58df5a21b
donatomontrone <donato.montrone@outlook.com>
2023-05-12 16:56:02 +0200

    fix whitespace removal

--- src/org/java/mymap/MainMap.java
+++ src/org/java/mymap/MainMap.java
@@ -10,14 +10,18 @@ public class MainMap {
 		String str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam - repudiandae est, alias in ullam excepturi ipsam modi odio Lorem ipsum dolor asperiores voluptatum, # quam distinctio ! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repudiandae Totam repudiandae est ; accusantium voluptatum, quam distinctio aut magnam quod veniam esse nesciunt debitis.";
 		
 		 str = str.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+		 
+		 System.out.println(str);
+		 
+		 String[] strArray = str.split(" ");
 	
 		
 		Map<String, Integer> words = new HashMap<>();
 		
 		
-		for (String word : Arrays.asList(str.split(" "))) {
+		for (String word : strArray) {
 			
-			
+			if (word.isBlank()) continue;
 			
 			 if (words.containsKey(word)) {
 				 
