ee2201ec899ae532cdaf1df4889b4b6f3b9c6c3d
donatomontrone <donato.montrone@outlook.com>
2023-05-12 12:04:34 +0200

    added wish list in main class

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,5 +1,44 @@
 package org.lessons.java.christmas;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		List<String> wishList = new ArrayList<>();
+		Scanner in = new Scanner(System.in);
+		int choise = 0;
+		
+		while (choise != 2) {
+			
+			System.out.println("Cosa vuoi fare?");
+			System.out.println("1 - Inserisci un desiderio");
+			System.out.println("2 - Termina");
+			
+			choise = in.nextInt();
+			
+			if (choise !=1 ) continue;
+			
+			
+			
+			in.nextLine();
+			
+			System.out.println("Inserisci un desiderio..");
+			String wish = in.nextLine();
+			
+			wishList.add(wish);
+			
+			
+			System.out.println("La lista contiene attualmente: " + wishList.size() + " desideri.");
+			
+			
+			
+		}
+		
+		in.close();
+		System.out.println("Ecco la lista dei desideri");
+		System.out.println(wishList);
+	}
 }
