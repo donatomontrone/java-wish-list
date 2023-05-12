8a19e1d1ca24d0d30cdbe1c6e766526daa2b3c4e
donatomontrone <donato.montrone@outlook.com>
2023-05-12 15:20:16 +0200

    complete map exercise

---
+++ src/org/java/mymap/MainMap.java
@@ -0,0 +1,37 @@
+package org.java.mymap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainMap {
+	public static void main(String[] args) {
+		
+		String str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam - repudiandae est, alias in ullam excepturi ipsam modi odio Lorem ipsum dolor asperiores voluptatum, # quam distinctio ! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam repudiandae Totam repudiandae est ; accusantium voluptatum, quam distinctio aut magnam quod veniam esse nesciunt debitis.";
+		
+		 str = str.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+	
+		
+		Map<String, Integer> words = new HashMap<>();
+		
+		
+		for (String word : Arrays.asList(str.split(" "))) {
+			
+			
+			
+			 if (words.containsKey(word)) {
+				 
+				words.put(word, words.get(word) + 1);
+				
+			 } else {			
+				 
+				 words.put(word, 1);	 
+			 }
+			 
+			 words.remove("");
+		}
+		
+		System.out.println(words);
+
+	}
+}
